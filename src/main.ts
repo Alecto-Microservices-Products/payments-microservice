4bb8dd0391b407ca338cb41b0959fe1a8a635f15
Harry Ore Lara <harryorelara@gmail.com>
2024-05-11 13:10:54 -0500

    Primer commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+
+  const logger= new Logger('Payments-ms');
+
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(envs.port);
+
+
+  logger.log(`Payments microservice corriendo en el puerto ${envs.port}`)
+}
+bootstrap();
---
+++ src/payments/payments.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession(){
+    const session = await this.stripe.checkout.sessions.create({
+        //Colocar aqui el id de mi orden
+        payment_intent_data:{
+            metadata: {}
+        },
+        line_items:[
+            {
+                price_data:{
+                    currency: 'usd',
+                    product_data:{
+                        name:'T-Shirt'
+                    },
+                    unit_amount: 2000
+                },
+                quantity: 2,
+
+            }
+        ],
+        mode: 'payment',
+        success_url: 'http://localhost:3003/payments/success',
+        cancel_url: 'http://localhost:3003/payments/cancel'
+    });
+
+    return session;
+  }
+
+
+}
